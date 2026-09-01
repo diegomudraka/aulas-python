99c2bb66e10cfbe2016932595534c24c131c0e9a
diegomudraka <mudrakalive@gmail.com>
2026-09-01 19:57:29 -0300

    Conteudo de operadores aritimeticos

---
+++ aulas/OperadoresAritimeticos.py
@@ -0,0 +1,25 @@
+#Operadores matematicos
+
+numero1 = 10
+numero2 = 20
+
+#Soma
+soma = numero1 + numero2
+
+#subtração
+subtracao = numero1 = numero1 - numero2
+
+#Multiplicação
+multiplicacao = numero1 = numero1 * numero2
+
+#Divisão
+divisao = numero1 = numero1 / numero2
+resto = numero1 % numero2
+
+print("Soma dos numeros: ", soma)
+print("Subtração dos numeros: ", subtracao)
+print("Multiplicação dos numeros: ", multiplicacao)
+print("Divisão dos numeros: ", divisao)
+print("Resto dos numeros: ", resto)
+
+
